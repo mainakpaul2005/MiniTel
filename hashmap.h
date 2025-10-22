92be6f393dac3fda95685c0a8db36bf0c241f2e9
mainakpaul2005 <mainakpaul2005@gmail.com>
2025-10-22 23:51:09 +0530

    Clean Slate

--- hashmap.h
+++
@@ -1,9 +0,0 @@
-#ifndef HASHMAP_H
-#define HASHMAP_H
-
-unsigned int hash(const char* key);
-void insertHash(const char* key, int index);
-int searchHash(const char* key);
-void clearHash();
-
-#endif
